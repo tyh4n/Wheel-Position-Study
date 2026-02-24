59587c9a8a0630852dc7a6ca9c5719570daeb3e0
tyh4n <tianyih4@illinois.edu>
2026-02-23 18:17:23 -0600

    [SW] 022326 update

---
+++ mod_motor_config_optimization.m
@@ -0,0 +1,63 @@
+% Created by Tianyi Han, Jun.19 2023
+
+%% Initialize
+tic;
+clear;
+
+%% Robot parameters
+% Create empty dictionary
+params = dictionary(string([]), []);
+% Set values to attributes
+params("I_ball") = 0.052;     % Ball inertia [kgm^2]
+params("m_ball") = 4.0;       % Ball mass[kg]
+params("I_body") = 3.0;       % Body inertia [kgm^2]
+params("m_body") = 92;        % Body mass [kg] 
+params("r") = 0.114;          % Ball radius [m]
+params("l") = 0.45;           % Body length (IP) [m]
+params("g") = 9.81;           % [m/s^2]
+% [NOTE] if parameters above is changed, rerun trajectory optimization
+
+params("f_ball_arrestor") = 0;  % [N]
+params("k_wheel_ball") = 1e6;   % [N/m]
+% Torque distribution: 0 for conventional, 1 for equal ros
+params("torque_distribution") = 0;
+% Symmetric type: 0 for center symmetric, 1 for mirror symmetric
+params("symmetric_type") = 1;
+
+%% Loop through motor configs
+alpha = linspace(45, 45, 1)./(180/pi);      % [rad]
+beta = linspace(0, 15, 4)./(180/pi);      % [rad]
+gamma = linspace(0, 360, 37)./(180/pi);       % [rad]
+
+max_ros_map = zeros([length(beta), length(gamma)]);
+
+for alpha_idx = 1:length(alpha)
+    for beta_idx = 1:length(beta)
+        for gamma_idx = 1:length(gamma)
+            disp("-------------------------------");
+            disp("Current config: ")
+            disp([alpha(alpha_idx).*(180/pi), beta(beta_idx).*(180/pi), gamma(gamma_idx).*(180/pi)]);
+
+            params("alpha") = alpha(alpha_idx);
+            params("beta") = beta(beta_idx);
+            params("gamma") = gamma(gamma_idx);
+
+            [max_ros] = ros_calculation(params);
+            max_ros_map(beta_idx, gamma_idx) = max_ros;
+            disp(['Max risk of slip: ', num2str(max_ros)]);
+        end
+    end
+end
+
+%% Save data 
+save('data/ros_map_022227_mirror.mat', 'alpha', 'beta', 'gamma', 'max_ros_map');
+
+%% Plot results
+% Plot for center symmetric configuration
+polarplot(gamma, max_ros_map, 'LineWidth', 2);
+
+
+%% Show total running time
+toc;
+
+
