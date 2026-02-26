addeeb4944638bbbe0c6db4ee6b5c67832a57823
tyh4n <tianyih4@illinois.edu>
2026-02-26 15:11:46 -0600

    [SW] revise motor speed constrain and results

--- traslation_speed_constrain.m
+++ traslation_speed_constrain.m
@@ -17,8 +17,8 @@ params("symmetric_type") = 1;
 
 %% Loop through motor configs
 % Set alpha, beta, gamma angle
-alpha = 45./(180/pi);                       % [rad]
-beta = 10./(180/pi);                        % [rad]
+alpha = 45./(180/pi);                         % [rad]
+beta = linspace(0, 15, 4)./(180/pi);          % [rad]
 gamma = linspace(0, 360, 361)./(180/pi);      % [rad]
 % Assume in upright position
 phi = 0;                                    % [rad]
@@ -27,49 +27,52 @@ v_motor_lim = 2.553;                 % [m/s]
 % Velocity increment step
 v_step = 1e-2;
 
-max_velocity_map = zeros(1, length(gamma));
+max_velocity_map = zeros(length(beta), length(gamma));
 
-for gamma_idx = 1:length(gamma)
-    % Init
-    v_motor_max = 0;
-    v_ball_tar = 0;
-
-    while (v_motor_max <= v_motor_lim)
-        v_ball_tar = v_ball_tar + v_step;
-
-        params("alpha") = alpha;
-        params("beta") = beta;
-        params("gamma") = gamma(gamma_idx);
-
-        [r_vec_world, R_wheel2world] = rotation_matrix(phi, params);
-
-        % Calculet ball angular velocity in world frame
-        w_ball = [-v_ball_tar / params("r"); 0; 0];
-
-        % Calculate wheel unit vector in world frame
-        uv_wheel_frame = {[], [], [], []};
-        uv_world_frame = {[], [], [], []};
-        for i = 1:4
-            uv_wheel_frame{i} = [1; 0; 0];
-            uv_world_frame{i} = R_wheel2world{i} * uv_wheel_frame{i};
+for beta_idx = 1:length(beta)
+    for gamma_idx = 1:length(gamma)
+        % Init
+        v_motor_max = 0;
+        v_ball_tar = 3;
+    
+        while (v_motor_max <= v_motor_lim) 
+            v_ball_tar = v_ball_tar + v_step;
+    
+            params("alpha") = alpha;
+            params("beta") = beta(beta_idx);
+            params("gamma") = gamma(gamma_idx);
+    
+            [r_vec_world, R_wheel2world] = rotation_matrix(phi, params);
+    
+            % Calculet ball angular velocity in world frame
+            w_ball = [-v_ball_tar / params("r"); 0; 0];
+    
+            % Calculate wheel unit vector in world frame
+            uv_wheel_frame = {[], [], [], []};
+            uv_world_frame = {[], [], [], []};
+            for i = 1:4
+                uv_wheel_frame{i} = [1; 0; 0];
+                uv_world_frame{i} = R_wheel2world{i} * uv_wheel_frame{i};
+            end
+    
+            % Calculate motor velocity in world frame
+            v_motor = {[], [], [], []};
+            for i = 1:4
+                v_contact = cross(w_ball, r_vec_world{i});
+                v_motor{i} = norm(dot(v_contact, uv_world_frame{i}));
+            end
+    
+            % Calculate max motor velocity
+            v_motor_max = max(max(v_motor{1}, v_motor{2}), max(v_motor{3}, v_motor{4}));
         end
+        
 
-        % Calculate motor velocity in world frame
-        v_motor = {[], [], [], []};
-        for i = 1:4
-            v_contact = cross(w_ball, r_vec_world{i});
-            v_motor{i} = norm(dot(v_contact, uv_world_frame{i}));
-        end
-
-        % Calculate max motor velocity
-        v_motor_max = max(max(v_motor{1}, v_motor{2}), max(v_motor{3}, v_motor{4}));
+        max_velocity_map(beta_idx,gamma_idx) = v_ball_tar;
     end
-
-    max_velocity_map(gamma_idx) = v_ball_tar;
 end
 
 %% Save data 
-save('data/max_velocity_map_022825_mirror.mat', 'alpha', 'beta', 'gamma', 'max_velocity_map');
+save('data/v_map_022626_mirror.mat', 'alpha', 'beta', 'gamma', 'max_velocity_map');
 
 %%  Plot
 figure(1);
